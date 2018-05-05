a40601a2d53cfa8363f2d00d6daf3333f6e4c66b
dreazz <agustinsilveira.96@gmail.com>
2018-05-06 00:05:38 +0200

    Todo comentado, aun se puede mejorar el programa. Tomar un momento para darle los ultimos retoques de funcionalidad

--- src/EmailApp/Email.java
+++ src/EmailApp/Email.java
@@ -23,23 +23,28 @@ public class Email {
 	//Creating constructors 
 	//Constructor lastName and firstName
 	public Email(String firstName , String lastName) {
+		//cogemos mediante el input del usuario el nombre y el apellido
 		this.firstName=firstName.toLowerCase();
 		this.lastName=lastName.toLowerCase();
-		System.out.println("Email works: "+ this.firstName + this.lastName+"");
-		//Declaramos que department es igual a la clase setDepartment para poder acceder a el
+		System.out.println("Welcome "+ this.firstName + this.lastName+"");
+		//Declaramos que department es igual a la clase setDepartment para poder acceder a el y pasamos los valores a minusculas
 		this.department=setDepartment().toLowerCase();
+		//agregamos el domain de la empresa, lo inicializamos aqui por que arriba no pilla department
 		this.companyDomain="@"+this.department+"company.com";
 		System.out.println("Department: "+this.department);
+		//aqui le pasamos al parametro del method randomPassword el length definido al principio
 		this.password=randomPassword(passwordLength);
-		
+		//SIntaxis del email
 		this.email=this.firstName+this.lastName+this.companyDomain;
 		System.out.println(this.email);
 		
 	}
-
+//Private method donde procesamos el department del usuario
 	private String setDepartment() {
 		System.out.print("Choose your Department:\n1)Sales\n2)Development\n3)Accounting\n0)None\nAnswer: ");
+		//Usamos scanner y lo definimos
 		Scanner in= new Scanner(System.in);
+		//inicializamos el depChoice para que coja el valor de la variable in de tipo scanner y decimos que recibirá un entero
 		int depChoice=in.nextInt();
 		if(depChoice == 1 ) {
 		return "Sales.";
@@ -52,12 +57,19 @@ public class Email {
 		}
 		
 	}
+	//Metodo privado para la creación de una random password 
 	private String randomPassword(int length) {
+		//definimos una variable donde definimos todos los caracteres que usaran nuestra password
 		String passwordSet="ABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890@/()!";
+		// definimos un array de caracteres que será igual de grande que el length
 		char[] password=new char[length];
+		//Bucle for para recorrer la string
 		for(int i=0; i<length;i++) {
-			
+			//definimos una variable que guardara un valor entero y que será la posición de un caracter de la string
+			//casteamos el resultado del math.random como (int) para despues poder pasarlo a la variable
 			int ran=(int) (Math.random()*passwordSet.length());
+			//guardamos en password en la posición [i]  hasta un máximo de length, cada caracter de la string 
+			//que marque la variable ran que recibia un valor random es seleccionado estudiando cada caracter con el charAt(). 
 			password[i]=passwordSet.charAt(ran);
 		}
 		System.out.println(password);
@@ -72,6 +84,7 @@ public class Email {
 	public void alternateEmail(String altEmail) {
 		this.alternateEmail=altEmail;
 	}
+	//Change password method
 	public void changePassword(String password) {
 		this.password=password;
 		
@@ -82,6 +95,10 @@ public class Email {
 	public String getAlternateEmail() {return alternateEmail;}
 	public String getPassword() {return password;}
 	
+	//Show method, will be used to show all the user info
+	public String showInfo() {
+		return "User info: "+firstName+" "+lastName+"\nEmail: "+email;
+	}
 
 
 
